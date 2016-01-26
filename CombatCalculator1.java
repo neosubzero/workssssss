519f929dd0ec55ff42fbcda4d5716fcd9a8d1fc3
neosubzero <elcarte1@uno.edu>
2016-01-26 21:42:22 +0000

    added combatcalculator1

---
+++ CombatCalculator1.java
@@ -0,0 +1,21 @@
+public class CombatCalculator1
+{
+    public static void main(String[] args)
+    { 
+        /*Monster data variables*/
+        //Declare variable for monster's name and initialize it to “goblin”
+        String MonsterName = "goblin";
+        //Declare variable for monster's health and initialize it 100
+        int MonsterHealth = 100;
+        //Declare variable for monster's attack power and initialize it to 15
+        int MonsterAttackPower = 15;
+        
+        /*Hero data variables*/
+        //Declare variable for Hero's health and initialize it to 100
+        int HeroHealth = 100;
+        //Declare variable for Hero's attack power and initialize it to 12
+        int HeroAttackPower = 12;
+        //Declare variable for Hero's magic power and initialize it to 0
+        int HeroMagicPower = 0;
+    }
+}
